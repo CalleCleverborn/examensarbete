d984e61d2defdc853ce81bbe09293d6fadfe44f8
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-12-17 13:39:08 +0100

    fix: move index to src folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import session from 'express-session';
-import passport from './src/auth/passport'
-import authRouter from './src/routes/auth'
+import passport from './auth/passport';
+
+import authRouter from './routes/auth';
+import transformationsRouter from './routes/transformations';
 
 const port = process.env.PORT || 4000;
 const mongodbUri = process.env.MONGODB_URI;
@@ -24,13 +27,14 @@ app.use(cors({
   origin: 'http://localhost:5173', 
   credentials: true
 }));
+
 app.use(express.json());
 
 app.use(session({
   secret: 'supersecretkey', 
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true}
+  cookie: { secure: false } 
 }));
 
 app.use(passport.initialize());
@@ -41,7 +45,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRouter);
+app.use('/api/transformations', transformationsRouter);
 
 app.listen(port, () => {
-  console.log(`Backend listening on localhost:${port}`);
+  console.log(`Backend listening on port ${port}`);
 });

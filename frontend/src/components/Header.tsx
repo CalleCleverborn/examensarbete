639551f2622ebb2934ae12fcf69643d3637375c8
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-12-31 20:20:36 +0100

    feat: header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./_Header.scss";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>VocalFlow</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/app/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/app/my-plan">My Plan</Link>
+          </li>
+          <li>
+            <Link to="/app/about">About</Link>
+          </li>
+          <li>
+            <Link to="#">Logout</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

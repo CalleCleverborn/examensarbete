1c07f371705b52ed1a5c9a5e97c6e0fccf36b8e4
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-12-31 20:21:23 +0100

    feat: styling

---
+++ frontend/src/pages/MyPlan.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyPlan: React.FC = () => {
+  return (
+    <div>
+      <h2>Plan</h2>
+      <p>Account details go here...</p>
+    </div>
+  );
+};
+
+export default MyPlan;

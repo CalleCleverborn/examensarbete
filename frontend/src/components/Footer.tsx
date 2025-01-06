a00397fa64cb51634a764a65ecfb12535f4a605c
Carl Cleverborn <carl.cleverborn@hotmail.se>
2025-01-06 17:08:17 +0100

    feat: footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import "./_Footer.scss";
+
+export const Footer = () => {
+  return <div className="footer-wrapper">Footer</div>;
+};
+
+export default Footer;
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 import "./_Layout.scss";
 
@@ -11,6 +12,7 @@ const Layout: React.FC = () => {
       <div className="content-wrapper">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };

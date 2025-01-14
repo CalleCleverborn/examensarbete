9ecdfc8819adccc4f3dfd460a7e50200671d45a7
Carl Cleverborn <carl.cleverborn@hotmail.se>
2025-01-14 23:13:29 +0100

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import passport from './auth/passport';
-import plansRouter from './routes/plans';
-import authRouter from './routes/auth';
-import transformationsRouter from './routes/transformations';
-import stripeRouter from './routes/stripe';
-import { seedPlans } from './scripts/seedPlans'; 
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "./auth/passport";
+import plansRouter from "./routes/plans";
+import authRouter from "./routes/auth";
+import transformationsRouter from "./routes/transformations";
+import stripeRouter from "./routes/stripe";
+import { seedPlans } from "./scripts/seedPlans";
 
 const port = process.env.PORT || 4000;
 const mongodbUri = process.env.MONGODB_URI;
@@ -29,37 +29,39 @@ mongoose
 
 const app = express();
 
-app.use(session({
-  secret: 'supersecretkey',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false, 
-    sameSite: 'none' 
-  }
-}))
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://vocalflow.netlify.app", 
+    credentials: true,
+  })
+);
+
 
 app.use(
   session({
-    secret: 'supersecretkey',
+    secret: "supersecretkey",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }, 
+    cookie: {
+      secure: true,     
+      sameSite: "none", 
+    },
   })
 );
+
+app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('Backend API is running!');
+app.get("/", (req, res) => {
+  res.send("Backend API is running!");
 });
 
-app.use('/auth', authRouter);
-app.use('/api/transformations', transformationsRouter);
-app.use('/api/plans', plansRouter);
-app.use('/api/stripe', stripeRouter);
+app.use("/auth", authRouter);
+app.use("/api/transformations", transformationsRouter);
+app.use("/api/plans", plansRouter);
+app.use("/api/stripe", stripeRouter);
 
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}`);
